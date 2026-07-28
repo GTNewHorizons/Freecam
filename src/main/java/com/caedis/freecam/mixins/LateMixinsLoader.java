4d183614c8f5cbebeb005a3d190fab373ae777bd
Caedis <Caedis@users.noreply.github.com>
2026-07-27 21:20:08 -0500

    Change modid

--- src/main/java/com/caedis/freecam/mixins/LateMixinsLoader.java
+++ src/main/java/com/caedis/freecam/mixins/LateMixinsLoader.java
@@ -14,7 +14,7 @@ public class LateMixinsLoader implements ILateMixinLoader {
 
     @Override
     public String getMixinConfig() {
-        return "mixins.freecam.late.json";
+        return "mixins.freecam-gtnh.late.json";
     }
 
     @Nonnull
